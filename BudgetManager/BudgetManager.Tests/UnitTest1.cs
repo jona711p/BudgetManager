ac0c56a3fe5c065a138bdff1c5529f0bd7a6cf69
Jonas Thorhauge Lykke <jona711p@student.eadania.dk>
2017-10-06 09:53:23 +0200

    Test

--- BudgetManager/BudgetManager.Tests/UnitTest1.cs
+++ BudgetManager/BudgetManager.Tests/UnitTest1.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestProjectLibrary;
 
-namespace BudgetManager.Tests
+namespace UnitTest
 {
+
     [TestClass]
-    public class UnitTest1
+    public class UnitTest
     {
+        testClass objt;
+        string rvalue;
+
+        [TestCleanup]
+        public void testClean()
+        {
+            objt = null;
+            rvalue = String.Empty;
+        }
+
+        [TestInitialize]
+        public void testInit()
+        {
+            objt = new testClass();
+            rvalue = "done";
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(rvalue, objt.testFun());
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(rvalue, objt.testFun());
         }
     }
 }
