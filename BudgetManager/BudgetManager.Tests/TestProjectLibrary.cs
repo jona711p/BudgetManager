19556bbe1a92746966ba5015fd5b7431fbc9e794
Mikael David Paaske <mika2466@student.eadania.dk>
2017-10-06 09:24:29 +0200

    Unittest

---
+++ BudgetManager/BudgetManager.Tests/TestProjectLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectLibrary
+{
+    public class testClass
+    {
+        public string testFun()
+        {
+            return "done";
+        }
+    }
+}
