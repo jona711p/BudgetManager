1174834cb166c1608be75a5aaa136230749062c6
Jonas Thorhauge Lykke <jona711p@student.eadania.dk>
2017-10-06 09:52:15 +0200

    Edit, Delete, Max Lengh

--- BudgetManager/BudgetManager/Models/Budgets.cs
+++ BudgetManager/BudgetManager/Models/Budgets.cs
@@ -12,22 +12,23 @@ namespace BudgetManager.Models
 
         [Display(Name = "Budget Navn")]
         [Required(ErrorMessage = "Du skal anføre et budget navn")]
+        [StringLength(100, ErrorMessage = "Navnet på budgettet må max være på 100 tegn")]
         public string BudgetName { get; set; }
 
         [Display(Name = "Formål")]
         [Required(ErrorMessage = "Du skal anføre et formål")]
+        [StringLength(400, ErrorMessage = "Formålsbeskrivelsen må max være på 400 tegn")]
         public string Purpose { get; set; }
 
         [Display(Name = "Regnskabsår")]
         [Required(ErrorMessage = "Du skal anføre et regnskabsår")]
+        [Range(1000, 9999)]
         public int FiscalYear { get; set; }
 
-        [Display(Name = "Synlighed")]
-        [Required(ErrorMessage = "Du skal anføre Synlighed")]
+        [Display(Name = "Offentlig")]
         public bool Visibility { get; set; }
 
         [Display(Name = "Interval")]
-        [Required(ErrorMessage = "Du skal anføre interval")]
         public int Interval { get; set; }
 
     }
